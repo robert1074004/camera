c81b359189bb554ed862841020cf0d776d734b01
robert1074004 <abc50113@yahoo.com.tw>
2022-05-13 23:42:03 +0800

    feat: show.handlebars

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port =3000
 const exphbs = require('express-handlebars')
+const { redirect } = require('express/lib/response')
 
-const camera_list = [{'相機':[{'name':'Canon 90D','image':'/images/Canon90D.jpg','price':1000},{'name':'Canon R5','image':'/images/CanonR5.jpg','price':1500},{'name':'Canon 850D','image':'/images/Canon850D.jpg','price':500}]}]
+const camera_list = [{'相機':[{'name':'Canon 90D','image':'/images/Canon90D.jpg','price':1000,'id':0},{'name':'Canon R5','image':'/images/CanonR5.jpg','price':1500,'id':1},{'name':'Canon 850D','image':'/images/Canon850D.jpg','price':500,'id':2}]}]
 
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
@@ -14,9 +15,9 @@ app.get('/',(req,res) => {
     res.render('index',{camera_list:camera_list[0].相機})
 })
 
-app.get('/show_select',(req,res) => {
-    console.log(req.query.length)
-    res.render('show_select')
+app.get('/show/:category',(req,res) => {
+    const category = camera_list[0].相機[Number(req.params.category)]
+    res.render('show',{category})
 })
 
 app.get('/sign_in',(req,res) => {
@@ -28,6 +29,11 @@ app.get('/log_in',(req,res) => {
 })
 
 app.get('/record',(req,res) => {
+    let now = new Date()
+    let myDate = new Date(req.query.time)
+    if ((myDate-now)/(1000 * 60 * 60 * 24) <= 1) {
+        res.redirect('/')
+}
     res.render('record')
 })
 
