c314501976eece9fd2a822142aa9b15a683341c6
robert1074004 <abc50113@yahoo.com.tw>
2022-05-13 15:34:00 +0800

    feat: remove index.js

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express()
 const port =3000
 const exphbs = require('express-handlebars')
 
+const camera_list = [{name:"相機",image:"/images/相機.jpg",price:1000,categoryFirst:"canon 90D",categorySecond:"canon 80D",categoryThird:"canon 60D"},{name:"腳架",image:"/images/腳架.jpg",price:500,categoryFirst:"腳架1",categorySecond:"腳架2",categoryThird:"腳架3"},{name:"攝影機",image:"/images/攝影機.jpg",price:1500,categoryFirst:"攝影機1",categorySecond:"攝影機2",categoryThird:"攝影機3"},{name:"記憶卡",image:"/images/記憶卡.jpg",price:100,categoryFirst:"記憶卡1",categorySecond:"記憶卡2",categoryThird:"記憶卡3"}]
+
 app.engine('handlebars',exphbs({defaultLayout:'main'}))
 app.set('view engine','handlebars')
 
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
-    res.render('index')
+    res.render('index',{camera_list:camera_list})
 })
 
 app.get('/show_select',(req,res) => {
