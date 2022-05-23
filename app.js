926572e524c923d81519c683fcaaab377f3ec6db
robert1074004 <abc50113@yahoo.com.tw>
2022-05-23 22:49:29 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 
-
 const express = require('express')
 const app = express()
 const port =3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const routes = require('./routes')
-mongoose.connect('mongodb+srv://root:abc83213@learning.lmzd7.mongodb.net/camera?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
 
+require('./config/mongoose')
 
-const db = mongoose.connection
-
-db.on('error',() => {
-    console.log('mongodb error!')
-})
-
-db.once('open',() => {
-    console.log('mongodb connected!')
-})
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','hbs')
 
---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://root:abc83213@learning.lmzd7.mongodb.net/camera?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const db = mongoose.connection
+
+db.on('error',() => {
+    console.log('mongodb error!')
+})
+
+db.once('open',() => {
+    console.log('mongodb connected!')
+})
+
+module.exports = db
