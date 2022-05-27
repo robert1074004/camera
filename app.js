34b5d75931ccb277a90951b7602d428881928e16
robert1074004 <abc50113@yahoo.com.tw>
2022-05-27 22:03:47 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 const express = require('express')
-const app = express()
-const port =3000
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const app = express()
+const port =3000
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
@@ -12,6 +13,12 @@ require('./config/mongoose')
 
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','hbs')
+
+app.use(session({
+    secret:'ThisIsMySecret',
+    resave:false,
+    saveUninitialized:true
+}))
 
 app.use(express.static('public'))
 
