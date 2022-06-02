b20fb8e6bd13a17547b5e63805095ddce8e3120d
robert1074004 <abc50113@yahoo.com.tw>
2022-06-02 22:53:06 +0800

    feat: add middleware & isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -7,41 +7,38 @@ const routes = require('./routes')
 const usePassport = require('./config/passport')
 const app = express()
 const port =3000
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of d6154ac (feat: add user authentication in todo and home routers)
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({ override: true })
   }
 require('./config/mongoose')
 
-console.log(process.env)
+
 
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
 app.set('view engine','hbs')
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> parent of d6154ac (feat: add user authentication in todo and home routers)
 app.use(session({
     secret:'ThisIsMySecret',
     resave:false,
     saveUninitialized:true
 }))
 
-<<<<<<< HEAD
-const usePassport = require('./config/passport')
 
-=======
->>>>>>> parent of d6154ac (feat: add user authentication in todo and home routers)
+
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({extended:true}))
 
 usePassport(app)
+
+app.use((req, res, next) => {
+    console.log(req.user)
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    next()
+  })
 
 app.use(routes)
 
