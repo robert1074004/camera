93bcab280980fc52e2682f2e0356c2b67a3fdfea
robert1074004 <abc50113@yahoo.com.tw>
2022-06-05 15:45:36 +0800

    feat: modify home routes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,7 +3,21 @@ const router = express.Router()
 const Camera = require('../../models/camera')
 
 router.get('/',(req,res) => {
-     Camera.find()
+     Camera.find({category:'相機'})
+            .lean()
+            .then(cameras =>  res.render('index',{cameras}))
+            .catch(error => console.log('error'))
+})
+
+router.get('/cameratripod',(req,res) => {
+     Camera.find({category:'腳架'})
+            .lean()
+            .then(cameras =>  res.render('index',{cameras}))
+            .catch(error => console.log('error'))
+})
+
+router.get('/camera',(req,res) => {
+     Camera.find({category:'攝影機'})
             .lean()
             .then(cameras =>  res.render('index',{cameras}))
             .catch(error => console.log('error'))
