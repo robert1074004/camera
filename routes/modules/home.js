1dcdc3eaf77f5c9eee35cde60e993dc2ebac1100
robert1074004 <abc50113@yahoo.com.tw>
2022-06-25 11:32:21 +0800

    feat: modify .hbs in views

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,21 +10,6 @@ router.get('/:category',(req,res) => {
             .catch(error => console.log('error'))
 })
 
-// router.get('/cameratripod',(req,res) => {
-//      Camera.find({category:'腳架'})
-//             .lean()
-//             .then(cameras =>  res.render('index',{cameras}))
-//             .catch(error => console.log('error'))
-// })
-
-// router.get('/camera',(req,res) => {
-//      Camera.find({category:'攝影機'})
-//             .lean()
-//             .then(cameras =>  res.render('index',{cameras}))
-//             .catch(error => console.log('error'))
-// })
-
-
 
 
 
