fd0791056407d00ab7cdf7819c8d6e84995e611b
robert1074004 <abc50113@yahoo.com.tw>
2023-02-16 20:36:52 +0800

    feat: add authenticatedAdmin function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,28 @@
-module.exports = {
-  authenticator: (req, res, next) => {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    req.flash('warning_msg', '請先登入才能使用')
+// module.exports = {
+//   authenticator: (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//       return next()
+//     }
+//     req.flash('warning_msg', '請先登入才能使用')
+//     res.redirect('/log_in')
+//   }
+// }
+const { ensureAuthenticated, getUser } = require('../helpers/auth-helper')
+const authenticated = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/log_in')
+}
+const authenticatedAdmin = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    if (getUser(req).isAdmin) return next()
+    res.redirect('/')
+  } else {
     res.redirect('/log_in')
   }
+}
+module.exports = {
+  authenticated,
+  authenticatedAdmin
 }
