dc961e550ade182d15f9a184021ccd56ad5ea5e0
robert1074004 <abc50113@yahoo.com.tw>
2023-02-16 20:12:27 +0800

    feat: add equipment model

---
+++ models/equipment.js
@@ -0,0 +1,29 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Equipment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  Equipment.init({
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    image: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Equipment',
+    tableName: 'Equipments',
+    underscored: true
+  })
+  return Equipment
+}
