2361e62554c807cf075d8c8780a454f3492092fc
robert1074004 <abc50113@yahoo.com.tw>
2023-02-21 17:37:38 +0800

    feat: add create equipment function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,6 +5,21 @@ const adminController = {
     Equipment.findAll({ raw: true })
       .then(equipments => res.render('admin/equipments', { equipments }))
       .catch(err => next(err))
+  },
+  createEquipment: (req, res, next) => {
+    res.render('admin/create-equipment')
+  },
+  postEquipment: (req, res, next) => {
+    const { name, category, price, quantity } = req.body
+    if (!name.trim() || !category.trim() || !price.trim() || !quantity.trim()) {
+      throw new Error('Some fields must be required !')
+    }
+    Equipment.create({ name, category, price, quantity })
+      .then(() => {
+        req.flash('success_msg', 'Equipment was successfully  created')
+        res.redirect('/admin/equipments')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
+router.get('/equipments/create', adminController.createEquipment)
+
 router.get('/equipments', adminController.getEquipments)
+
+router.post('/equipments', adminController.postEquipment)
 
 module.exports = router
