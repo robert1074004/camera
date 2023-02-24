5cb46c1270c8d14e393a68e5b3a13d914776563e
robert1074004 <abc50113@yahoo.com.tw>
2023-02-24 15:22:41 +0800

    feat: add record model

---
+++ migrations/20230224071311-create-record.js
@@ -0,0 +1,46 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Records', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_name: {
+        type: Sequelize.STRING
+      },
+      user_email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Records')
+  }
+}
