0662cfdaf4598bed24bcb315a2509c821c80ece1
robert1074004 <abc50113@yahoo.com.tw>
2023-05-24 10:49:46 +0800

    feat: add image to user model

--- migrations/20230407054511-add-deadline-to-records.js
+++ migrations/20230407054511-add-deadline-to-records.js
@@ -3,13 +3,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Records', 'deadline', {
-      type: Sequelize.Sequelize.DATEONLY
+      type: Sequelize.DATEONLY
     })
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Records', 'deadline', {
-      type: Sequelize.Sequelize.DATEONLY
+      type: Sequelize.DATEONLY
     })
   }
 }
