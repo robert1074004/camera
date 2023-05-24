74c0094d58cd23eb66658c8109ea49c8b8f66fcc
robert1074004 <abc50113@yahoo.com.tw>
2023-05-24 13:00:47 +0800

    feat: add user image function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,25 @@
 const bcrypt = require('bcryptjs')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const { User } = require('../models')
 const userController = {
   SignUpPage: (req, res) => { res.render('sign_up') },
   SignUp: (req, res, next) => {
     const { name, email, password } = req.body
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (user) throw new Error('此郵件已被註冊成功!')
-        return bcrypt.hash(password, 10)
+    const { file } = req
+    return Promise.all([imgurFileHandler(file), bcrypt.hash(password, 10)])
+      .then(([filePath, hash]) => {
+        return User.findOrCreate({
+          where: { email },
+          defaults: {
+            name,
+            email,
+            password: hash,
+            image: filePath || 'https://i.imgur.com/Qo3mXjE.jpeg'
+          }
+        })
       })
-      .then(hash => {
-        return User.create({ name, email, password: hash })
-      })
-      .then(() => {
+      .then(users => {
+        if (!users[1]) throw new Error('此郵件已被註冊成功!')
         req.flash('success_msg', '註冊成功!')
         res.redirect('/log_in')
       })
