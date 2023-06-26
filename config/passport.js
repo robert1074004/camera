8d01ffb80d79e927de15a69f56cf06e264f96826
robert1074004 <abc50113@yahoo.com.tw>
2023-06-26 15:27:43 +0800

    feat: add google login function

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,36 @@
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const { User } = require('../models')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'https://camera1074004.herokuapp.com/google/callback',
+  passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, done) => {
+  const data = profile._json
+  if (!data) {
+    return done(null, false, req.flash('error_msg', '查無此人的資料!'))
+  }
+  User.findOrCreate({
+    where: { email: data.email },
+    defaults: {
+      name: data.name,
+      email: data.email,
+      image: data.picture
+    }
+  })
+    .then((user) => {
+      return done(null, user[0])
+    })
+    .catch(err => done(err, false))
+}))
 
 passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password', passReqToCallback: true }, (req, email, password, done) => {
   User.findOne({ where: { email } })
